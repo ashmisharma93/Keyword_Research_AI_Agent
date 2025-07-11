e0839672edb3792b916fb795701f526472ec794c
Ashmita Sharma <151334724+ashmisharma93@users.noreply.github.com>
2025-07-11 12:29:35 +0530

    Update my_utils.py

--- scripts/my_utils.py
+++ scripts/my_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 def save_df(df, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     df.to_csv(path, index=False)
-    print(f"✅ Saved to {path}")
+    print(f"Saved to {path}")
 
 def print_top(df, n=10):
     print("\n Top", n, "keywords:")
